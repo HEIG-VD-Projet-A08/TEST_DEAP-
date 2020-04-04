9cde2ebc2754beb86c715d8f751b6c70d99a1c3e
Quentin Saucy <quentin.saucy@heig-vd.ch>
2020-04-04 13:42:05 +0200

    test

--- test.py
+++ test.py
@@ -1,26 +1,188 @@
-from deap import base, creator, tools, algorithms
+#!/usr/bin/python3
+#    This file is part of DEAP.
+#
+#    DEAP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of
+#    the License, or (at your option) any later version.
+#
+#    DEAP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with DEAP. If not, see <http://www.gnu.org/licenses/>.
+
+
+#    example which maximizes the sum of a list of integers
+#    each of which can be 0 or 1
+
 import random
+from random import randint
 
-IND_SIZE = 10
+from deap import base
+from deap import creator
+from deap import tools
 
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMin)
+creator.create("FitnessMax", base.Fitness, weights=(-1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
+proteinAlphabet = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+target = "YEYEYEYEYE"
+minSizeWord = 4
+maxSizeWord = 20
+individualSize = 20
+populationSize = 100
+
+
+def generateText(intervalMin, intervalMax):
+    size = randint(intervalMin, intervalMax)
+    string = ""
+    for i in range(0, size - 1):
+        string += proteinAlphabet[randint(0, len(proteinAlphabet) - 1)]
+    return string
+
+
+def compareString(individual, target):
+    counter = 0
+    for i in range(0, len(individual)):
+        if target == individual[i]:
+            counter += 1
+    return counter
+
 
 toolbox = base.Toolbox()
-toolbox.register("attribute", random.random)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attribute, n=IND_SIZE)
+
+# Attribute generator
+#                      define 'attr_bool' to be an attribute ('gene')
+#                      which corresponds to integers sampled uniformly
+#                      from the range [0,1] (i.e. 0 or 1 with equal
+#                      probability)
+toolbox.register("generateRandomTexte", generateText, minSizeWord, maxSizeWord)
+
+# Structure initializers
+#                         define 'individual' to be an individual
+#                         consisting of 100 'attr_bool' elements ('genes')
+toolbox.register("individual", tools.initRepeat, creator.Individual,
+                 toolbox.generateRandomTexte, individualSize)
+
+# define the population to be a list of individuals
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 
-def evaluate(individual):
-    return sum(individual),
+# the goal ('fitness') function to be maximized
+def evalOneMax(individual):
+    return compareString(individual,target),
 
 
+# ----------
+# Operator registration
+# ----------
+# register the goal / fitness function
+toolbox.register("evaluate", evalOneMax)
+
+# register the crossover operator
 toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.1)
+
+# register a mutation operator with a probability to
+# flip each attribute/gene of 0.05
+toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+
+# operator for selecting individuals for breeding the next
+# generation: each individual of the current generation
+# is replaced by the 'fittest' (best) of three individuals
+# drawn randomly from the current generation.
 toolbox.register("select", tools.selTournament, tournsize=3)
-toolbox.register("evaluate", evaluate)
-
-algorithms.eaSimple(toolbox.population(n=50), toolbox, 0.4, 0.5, 100)
 
 
+# ----------
+
+def main():
+    random.seed(64)
+
+    # create an initial population of 300 individuals (where
+    # each individual is a list of integers)
+
+    # CXPB  is the probability with which two individuals
+    #       are crossed
+    #
+    # MUTPB is the probability for mutating an individual
+    CXPB, MUTPB = 0.8, 0.02
+
+    print("Start of evolution")
+    pop = toolbox.population(n=populationSize)
+    # Evaluate the entire population
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+
+    print("  Evaluated %i individuals" % len(pop))
+
+    # Extracting all the fitnesses of
+    fits = [ind.fitness.values[0] for ind in pop]
+
+    # Variable keeping track of the number of generations
+    g = 0
+
+    # Begin the evolution
+    while max(fits) < individualSize and g < 1000:
+        # A new generation
+        g = g + 1
+        print("-- Generation %i --" % g)
+
+        # Select the next generation individuals
+        offspring = toolbox.select(pop, len(pop))
+        # Clone the selected individuals
+        offspring = list(map(toolbox.clone, offspring))
+
+        # Apply crossover and mutation on the offspring
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+
+            # cross two individuals with probability CXPB
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+
+                # fitness values of the children
+                # must be recalculated later
+                del child1.fitness.values
+                del child2.fitness.values
+
+        # for mutant in offspring:
+
+        # mutate an individual with probability MUTPB
+        # if random.random() < MUTPB:
+        # toolbox.mutate(mutant)
+        # del mutant.fitness.values
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        print("  Evaluated %i individuals" % len(invalid_ind))
+
+        # The population is entirely replaced by the offspring
+        pop[:] = offspring
+
+        # Gather all the fitnesses in one list and print the stats
+        fits = [ind.fitness.values[0] for ind in pop]
+
+        length = len(pop)
+        mean = sum(fits) / length
+        sum2 = sum(x * x for x in fits)
+        std = abs(sum2 / length - mean ** 2) ** 0.5
+
+        print("  Min %s" % min(fits))
+        print("  Max %s" % max(fits))
+        print("  Avg %s" % mean)
+        print("  Std %s" % std)
+
+    print("-- End of (successful) evolution --")
+
+    best_ind = tools.selBest(pop, 1)[0]
+    print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+
+
+if __name__ == "__main__":
+    main()
